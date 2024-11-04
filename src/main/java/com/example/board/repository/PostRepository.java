68efb43c124bc9e6ab1e6a37a818b10f4a910dea
TomomiSugawara <t.mom.505@gmail.com>
2024-11-04 14:28:55 +0900

    board2

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.example.board.repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.board.entity.Post;
@@ -7,4 +10,11 @@ import com.example.board.entity.Post;
  * 投稿のリポジトリー.
  */
 public interface PostRepository extends JpaRepository<Post, String> {
+	/**
+	 * IDで検索する
+	 * 
+	 * @param id ID
+	 * @return 投稿
+	 */
+	public Optional<Post> findById(String id);
 }
